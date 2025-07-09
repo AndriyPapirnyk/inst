0fa6f8d3b9a0c8f88f45357c4f1901b68cd0f502
AndriyPapirnyk <papirnykandriy@gmail.com>
2025-07-09 18:04:42 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   const handleSubmit = async (e) => {
     e.preventDefault()
     try {
-      const response = await axios.post('http://localhost:8000/login', {
+      const response = await axios.post('https://test-server-inst-3d77f555108a.herokuapp.com/login', {
         username,
         password
       })
